0ded6ad0aa8322cfcbd5ce0aa949e5254e78ec0b
GothamKnight17 <ajit734567@gmail.com>
2024-05-18 11:12:51 +0530

    18/05/24

---
+++ p1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class p1 {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 ="Ajit";
+        System.out.println(s1.concat(s2));
+    }
+}
